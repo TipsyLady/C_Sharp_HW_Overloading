8b2ff10cf2dcd1a022a23f6af71dd1fef6310a85
Soro_pz45@itstepru.onmicrosoft.com <bu_ha_ha@bk.ru>
2022-03-13 15:09:28 +0300

    Add project files.

---
+++ Money.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_HW_Overloading
+{
+    class Money
+    {
+        private int grivna { get; set; }
+        private int kopeyka { get; set; }
+
+        //Конструктор
+        public Money (int gri, int kop)
+        {
+            grivna = gri;
+            kopeyka = kop;
+        }
+
+        //Перегрузки операторов
+        //Сложение
+
+        public static Money operator+(Money a, Money b)
+        {
+            int Res = ((a.grivna + b.grivna) * 100) + a.kopeyka + b.kopeyka;
+            return new Money(Res / 100, Res % 100);
+        }
+
+        //Сравнение
+        public static bool operator<(Money a, Money b)
+        {
+            if (a.grivna < b.grivna)
+                return true;
+            else if (a.grivna > b.grivna)
+                return false;
+            else if (a.kopeyka < b.kopeyka)
+                return true;
+            else return false;
+            
+        }
+        public static bool operator>(Money a, Money b)
+        {
+            if (a.grivna < b.grivna)
+                return false;
+            else if (a.grivna > b.grivna)
+                return true;
+            else if (a.kopeyka < b.kopeyka)
+                return false;
+            else return true;
+
+        }
+
+        public static bool operator==(Money a, Money b)
+        {
+            if (a.grivna == b.grivna && a.kopeyka == b.kopeyka)
+                return true;
+            else return false;
+        }
+        public static bool operator !=(Money a, Money b)
+        {
+            if (a.grivna != b.grivna && a.kopeyka != b.kopeyka)
+                return true;
+            else return false;
+        }
+        //Вычитание
+        public static Money operator-(Money a, Money b)
+        {
+            if (a < b)
+            {
+                Console.WriteLine("Operation can not be executed");
+                return null;
+            }
+            else
+            {
+                int Res = ((a.grivna - b.grivna) * 100) + (a.kopeyka - b.kopeyka);
+                return new Money(Res / 100, Res % 100);
+            }
+        }
+        // Умножение
+        public static Money operator *(Money a, int b)
+        {
+            int Res = (a.grivna * 100 + a.kopeyka) * b;
+            return new Money(Res / 100, Res % 100);
+        }
+        // Деление
+        public static Money operator /(Money a, int b)
+        {
+            try
+            {
+                int Res = (a.grivna * 100 + a.kopeyka) / b;
+                return new Money(Res / 100, Res % 100);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero is restricted");
+                return new Money(a.grivna, a.kopeyka);
+            }
+            
+        }
+            //Увеличение на 1 копейку
+            public static Money operator++(Money a)
+        {
+            a.kopeyka += 1;
+            if (a.kopeyka>=100)
+            {
+                a.grivna += 1;
+                a.kopeyka = 0;
+            }
+            return new Money(a.grivna, a.kopeyka);
+        }
+
+        //Уменьшение на 1 копейку
+        public static Money operator --(Money a)
+        {
+            a.kopeyka -= 1;
+            if (a.kopeyka == 0)
+            {
+                a.grivna -= 1;
+                a.kopeyka = 99;
+            }
+            return new Money(a.grivna, a.kopeyka);
+        }
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+        public bool Equals(Money obj)
+        {
+            if (obj is Money)
+            {
+                Money a = (Money)obj;
+                return a.grivna == this.grivna && a.kopeyka == this.kopeyka;
+            }
+                return false;
+        }
+
+        //ToString метод
+        public override string ToString()
+        {
+            return $"{grivna}g. {kopeyka}k.";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_HW_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money a = new Money(20, 10);
+            Money b = new Money(10, 20);
+            int c = 3;
+            Console.Write("Сложение: ");
+            Console.WriteLine($"{a} + {b} = {a+b}");
+            Console.WriteLine();
+            Console.Write("Вычитание: ");
+            Console.WriteLine($"{a} - {b} = {a-b}");
+            Console.WriteLine();
+            Console.Write("Умножение на целое: ");
+            Console.WriteLine($"{a} * {c} = {a*c}");
+            Console.WriteLine();
+            Console.Write("Деление на целое: ");
+            Console.WriteLine($"{a} / {c} = {a/c}");
+            Console.WriteLine();
+            a++;
+            Console.WriteLine($"Увеличенное на 1: {a} ") ;
+            a--;
+            Console.WriteLine($"Уменьшенное на 1: {a} ");
+
+
+
+
+        }
+    }
+}
